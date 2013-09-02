935a4ddc414cd1b1050f93fca76910f8b3783322
Elis Axelsson <elis.axelsson@gmail.com>
2013-09-03 00:36:58 +0200

    Added a simple player class, and render it

--- nyancat_v2/game.js
+++ nyancat_v2/game.js
@@ -14,10 +14,18 @@ var env = {
  * newGame -- Resets values and stuff
  */
 function newGame() {
+	// Clear the sky
 	env.backgroundObjects = [];
+	// make a new sky
 	for(var i = 0; i < Number.random(100, 151); i++) {
 		new Star();
 	}
+
+	// Clear game objects
+	env.gameObjects = [];
+
+	// Make a player
+	new Player();
 }
 
 /**
@@ -32,6 +40,11 @@ window.addEvent('load', function() {
 	window.requestAnimationFrame(gameLoop);
 });
 
+// Record keypresses
+var keyStates = {};
+window.addEvent('keydown', function(e) { if(keyStates[e.key] !== null) keyStates[e.key] = true; });
+window.addEvent('keyup',   function(e) { delete keyStates[e.key]; });
+
 /**
  * BaseClass with basic creating of objects and basic structure
  */
@@ -41,6 +54,8 @@ var BaseClass = new Class({
 
 		this.x = x ? x : Number.random(0, env.width);
 		this.y = y ? y : Number.random(0, env.height);
+
+		this.lastUpdateTime = Date.now();
 	},
 	move: function() {
 		this.draw();
@@ -93,6 +108,45 @@ var Star = new Class({
 });
 
 /**
+ * Player Class, handles player
+ */
+var Player = new Class({
+	Extends: BaseClass,
+	initialize: function(x, y) {
+		this.parent(x, y);
+		this.speed = 200;
+	},
+	move: function() {
+		var delta = (Date.now() - this.lastUpdateTime) / 1000;
+
+		// Some simple movements
+		if(keyStates.up)    this.y -= this.speed * delta;
+		if(keyStates.down)  this.y += this.speed * delta;
+		if(keyStates.left)  this.x -= this.speed * delta;
+		if(keyStates.right) this.x += this.speed * delta;
+
+		// Some simple checks to keep it inside of the canvas
+		// @TODO: Do not use 30 here, use the players width
+		if(this.y < 0)               this.y = 0;
+		if(this.y > env.height - 30) this.y = env.height - 30;
+		if(this.x < 0)               this.x = 0;
+		if(this.x > env.width - 30)  this.x = env.width - 30;
+
+		// Store update time
+		this.lastUpdateTime = Date.now();
+
+		this.draw();
+	},
+	draw: function() {
+		var ctx = env.ctx.screen;
+
+		// @TODO: Render images insdead of a green cube :)
+		ctx.fillStyle = '#00ff00';
+		ctx.fillRect(this.x, this.y, 30, 30);
+	}
+});
+
+/**
  * gameLoop which renders stuff
  */
 function gameLoop() {
@@ -108,6 +162,14 @@ function gameLoop() {
 		});
 	}
 	env.backgroundFrame = !env.backgroundFrame; // Toggle frame for background
+
+	/**
+	 * Update Game Objects
+	 */
+	env.ctx.screen.clearRect(0, 0, env.width, env.height);
+	Array.each(env.gameObjects, function(object) {
+		object.move();
+	});
 
 	window.requestAnimationFrame(gameLoop);
 }
