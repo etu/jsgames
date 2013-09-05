1bd84b210d0febd7c018b42ac288c7797bde9d95
Elis Axelsson <elis.axelsson@gmail.com>
2013-09-06 00:58:15 +0200

    Added spritesheet of nyancat and implemented the animation

--- nyancat_v2/game.js
+++ nyancat_v2/game.js
@@ -77,15 +77,35 @@ var BaseClass = new Class({
 var Player = new Class({
 	Extends: BaseClass,
 	options: {
-		speed: 200
+		speed:  200,
+		spritemap: new Image(),
+		width:  68,
+		height: 40,
+		frame:  0,
+		frameDelta: 0,
+		frameChange: 50 / 1000,
+		frames: 5
 	},
 	initialize: function(options) {
 		this.setOptions(options);
+
+		this.options.spritemap.src = 'nyan.png';
 
 		this.parent(options);
 	},
 	move: function() {
 		var delta = (Date.now() - this.options.lastUpdateTime) / 1000;
+
+		if(this.options.frameDelta > this.options.frameChange) {
+			++this.options.frame;
+
+			if(this.options.frame > this.options.frames) {
+				this.options.frame = 0;
+			}
+
+			this.options.frameDelta = 0;
+		}
+		this.options.frameDelta += delta;
 
 		// Some simple movements
 		if(keyStates.up)    this.options.y -= this.options.speed * delta;
@@ -94,11 +114,10 @@ var Player = new Class({
 		if(keyStates.right) this.options.x += this.options.speed * delta;
 
 		// Some simple checks to keep it inside of the canvas
-		// @TODO: Do not use 30 here, use the players width
-		if(this.options.y < 0)               this.options.y = 0;
-		if(this.options.y > env.height - 30) this.options.y = env.height - 30;
-		if(this.options.x < 0)               this.options.x = 0;
-		if(this.options.x > env.width - 30)  this.options.x = env.width - 30;
+		if(this.options.y < 0)                                this.options.y = 0;
+		if(this.options.y > env.height - this.options.height) this.options.y = env.height - this.options.height;
+		if(this.options.x < 0)                                this.options.x = 0;
+		if(this.options.x > env.width  - this.options.width)  this.options.x = env.width - this.options.width;
 
 		// Store update time
 		this.options.lastUpdateTime = Date.now();
@@ -108,9 +127,19 @@ var Player = new Class({
 	draw: function() {
 		var ctx = env.ctx.screen;
 
-		// @TODO: Render images instead of a green cube :)
-		ctx.fillStyle = '#00ff00';
-		ctx.fillRect(this.options.x, this.options.y, 30, 30);
+		var frameoffset = this.options.frame * this.options.width;
+
+		ctx.drawImage(
+			this.options.spritemap, // Image
+			frameoffset,            // Source X Position (within image)
+			0,                      // Source Y Position (within image)
+			this.options.width,     // Source Width
+			this.options.height,    // Source Height
+			this.options.x,         // Canvas X
+			this.options.y,         // Canvas Y
+			this.options.width,     // Canvas Width
+			this.options.height     // Canvas Height
+		);
 	}
 });
 
