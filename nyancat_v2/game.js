83e8d6a0ca077bab7b87abcaa5e0e76bae70efcf
Elis Axelsson <elis.axelsson@gmail.com>
2013-09-09 00:03:43 +0200

    Implemented pause

--- nyancat_v2/game.js
+++ nyancat_v2/game.js
@@ -6,7 +6,8 @@ var env = {
 	starfield:   false, // Storage of the starfield master object
 	gameObjects: [],
 	width:       640,
-	height:      480
+	height:      480,
+	pause:       false
 };
 
 
@@ -14,13 +15,11 @@ var env = {
  * newGame -- Resets values and stuff
  */
 function newGame() {
-	env.starfield = new StarField();
-
 	// Clear game objects
 	env.gameObjects = [];
 
-	// Make a player
-	new Player();
+	env.starfield = new StarField(); // Replace Starfield
+	env.player    = new Player();    // Replace Player
 }
 
 
@@ -34,6 +33,16 @@ window.addEvent('load', function() {
 	newGame();
 
 	window.requestAnimationFrame(gameLoop);
+
+	// unpause button :)
+	$('unpause').addEvent('click', function(e) {
+		unpause();
+	});
+});
+
+
+window.addEvent('blur', function() {
+	pause();
 });
 
 
@@ -72,79 +81,6 @@ var BaseClass = new Class({
 
 
 /**
- * Player Class, handles player
- */
-var Player = new Class({
-	Extends: BaseClass,
-	options: {
-		speed:  200,
-		spritemap: new Image(),
-		width:  68,
-		height: 40,
-		frame:  0,
-		frameDelta: 0,
-		frameChange: 100 / 1000,
-		frames: 5
-	},
-	initialize: function(options) {
-		this.setOptions(options);
-
-		this.options.spritemap.src = 'nyan.png';
-
-		this.parent(options);
-	},
-	move: function() {
-		var delta = (Date.now() - this.options.lastUpdateTime) / 1000;
-
-		if(this.options.frameDelta > this.options.frameChange) {
-			++this.options.frame;
-
-			if(this.options.frame > this.options.frames) {
-				this.options.frame = 0;
-			}
-
-			this.options.frameDelta = 0;
-		}
-		this.options.frameDelta += delta;
-
-		// Some simple movements
-		if(keyStates.up)    this.options.y -= this.options.speed * delta;
-		if(keyStates.down)  this.options.y += this.options.speed * delta;
-		if(keyStates.left)  this.options.x -= this.options.speed * delta;
-		if(keyStates.right) this.options.x += this.options.speed * delta;
-
-		// Some simple checks to keep it inside of the canvas
-		if(this.options.y < 0)                                this.options.y = 0;
-		if(this.options.y > env.height - this.options.height) this.options.y = env.height - this.options.height;
-		if(this.options.x < 0)                                this.options.x = 0;
-		if(this.options.x > env.width  - this.options.width)  this.options.x = env.width - this.options.width;
-
-		// Store update time
-		this.options.lastUpdateTime = Date.now();
-
-		this.draw();
-	},
-	draw: function() {
-		var ctx = env.ctx.screen;
-
-		var frameoffset = this.options.frame * this.options.width;
-
-		ctx.drawImage(
-			this.options.spritemap, // Image
-			frameoffset,            // Source X Position (within image)
-			0,                      // Source Y Position (within image)
-			this.options.width,     // Source Width
-			this.options.height,    // Source Height
-			this.options.x,         // Canvas X
-			this.options.y,         // Canvas Y
-			this.options.width,     // Canvas Width
-			this.options.height     // Canvas Height
-		);
-	}
-});
-
-
-/**
  * gameLoop which renders stuff
  */
 function gameLoop() {
@@ -152,13 +88,45 @@ function gameLoop() {
 	env.starfield.move();
 
 	/**
-	 * Update Game Objects
+	 * Clear entire screen
 	 */
 	env.ctx.screen.clearRect(0, 0, env.width, env.height);
+
+	// Draw player
+	env.player.move();
+
+	// Draw other game objects
 	Array.each(env.gameObjects, function(object) {
 		object.move();
 	});
 
+	if(!env.pause) {
+		window.requestAnimationFrame(gameLoop);
+	}
+}
+
+/**
+ * Function to unpause the game when it's paused
+ */
+function unpause() {
+	var time = Date.now();
+
+	$('pausescreen').setStyle('display', 'none');
+
+	// update timers
+	env.player.options.lastUpdateTime = time;
+	env.starfield.options.lastUpdateTime = time;
+
+	env.pause = false;
+
 	window.requestAnimationFrame(gameLoop);
 }
 
+/**
+ * Function to pause the game
+ */
+function pause() {
+	$('pausescreen').setStyle('display', 'block');
+
+	env.pause = true;
+}
---
+++ nyancat_v2/player.js
@@ -0,0 +1,78 @@
+
+/**
+ * Player Class, handles player
+ */
+var Player = new Class({
+	Implements: [ Options ],
+	options: {
+		speed:  200,
+		spritemap: new Image(),
+		width:  68,
+		height: 40,
+		frame:  0,
+		frameDelta: 0,
+		frameChange: 100 / 1000,
+		frames: 5,
+		lastUpdateTime: Date.now(),
+		x: 0,
+		y: 0
+	},
+	initialize: function(options) {
+		// Override some default options, if I put the random operations
+		// in the options object, it will be the same random for all objects :-)
+		this.options.x = Number.random(0, env.width);
+		this.options.y = Number.random(0, env.height);
+
+		this.setOptions(options);
+
+		this.options.spritemap.src = 'nyan.png';
+	},
+	move: function() { // Handle movement and animation of the player
+		var delta = (Date.now() - this.options.lastUpdateTime) / 1000;
+
+		if(this.options.frameDelta > this.options.frameChange) {
+			++this.options.frame;
+
+			if(this.options.frame > this.options.frames) {
+				this.options.frame = 0;
+			}
+
+			this.options.frameDelta = 0;
+		}
+		this.options.frameDelta += delta;
+
+		// Some simple movements
+		if(keyStates.up)    this.options.y -= this.options.speed * delta;
+		if(keyStates.down)  this.options.y += this.options.speed * delta;
+		if(keyStates.left)  this.options.x -= this.options.speed * delta;
+		if(keyStates.right) this.options.x += this.options.speed * delta;
+
+		// Some simple checks to keep it inside of the canvas
+		if(this.options.y < 0)                                this.options.y = 0;
+		if(this.options.y > env.height - this.options.height) this.options.y = env.height - this.options.height;
+		if(this.options.x < 0)                                this.options.x = 0;
+		if(this.options.x > env.width  - this.options.width)  this.options.x = env.width - this.options.width;
+
+		// Store update time
+		this.options.lastUpdateTime = Date.now();
+
+		this.draw();
+	},
+	draw: function() { // Draw the player on the screen
+		var ctx = env.ctx.screen;
+
+		var frameoffset = this.options.frame * this.options.width;
+
+		ctx.drawImage(
+			this.options.spritemap, // Image
+			frameoffset,            // Source X Position (within image)
+			0,                      // Source Y Position (within image)
+			this.options.width,     // Source Width
+			this.options.height,    // Source Height
+			this.options.x,         // Canvas X
+			this.options.y,         // Canvas Y
+			this.options.width,     // Canvas Width
+			this.options.height     // Canvas Height
+		);
+	}
+});
